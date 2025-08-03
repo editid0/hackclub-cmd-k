4f360bf66380f7b7661262206ceca7649aaa245f
editid0 <slyfoxdiscordbot@gmail.com>
2025-08-03 18:50:30 +0100

    remove unused characters

--- src/modutools.jsx
+++ src/modutools.jsx
@@ -4,19 +4,11 @@ const Fuse = require('fuse.js')
 
 const fuseOptions = {
     isCaseSensitive: false,
-    // includeScore: false,
-    // ignoreDiacritics: false,
     shouldSort: true,
-    // includeMatches: false,
-    // findAllMatches: false,
-    // minMatchCharLength: 1,
-    // location: 0,
     threshold: 0.4,
     distance: 100,
     useExtendedSearch: false,
     ignoreLocation: false,
-    // ignoreFieldNorm: false,
-    // fieldNormWeight: 1,
     keys: [
         "name",
         "description",
